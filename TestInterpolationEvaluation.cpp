82d0137c7d70040119882455373e60c297f74226
Sachith Dunatunga <neocpp@gmail.com>
2015-06-08 16:29:53 -0400

    Added tests for horner forms of other functions.

--- TestInterpolationEvaluation.cpp
+++ TestInterpolationEvaluation.cpp
@@ -18,3 +18,77 @@ TEST_CASE("Interpolation Evaluation (w1)", "[interpolations][short]") {
     }
 }
 
+TEST_CASE("Interpolation Evaluation (w2)", "[interpolations][short]") {
+    const int Nr = 100;
+    const int Nt = 50;
+    const double dr = 1.0 / Nr;
+    const double dt = 2 * M_PI / Nt;
+
+    for(int i = 0; i < Nr; i++) {
+        for (int j = 0; j < Nt; j++) {
+            const double r = i * dr;
+            const double t = j * dt;
+            CHECK(w2_naive(r, t) == Approx(w2_horner(r, t)));
+        }
+    }
+}
+
+TEST_CASE("Interpolation Evaluation (w3)", "[interpolations][short]") {
+    const int Nr = 100;
+    const int Nt = 50;
+    const double dr = 1.0 / Nr;
+    const double dt = 2 * M_PI / Nt;
+
+    for(int i = 0; i < Nr; i++) {
+        for (int j = 0; j < Nt; j++) {
+            const double r = i * dr;
+            const double t = j * dt;
+            CHECK(w3_naive(r, t) == Approx(w3_horner(r, t)));
+        }
+    }
+}
+
+TEST_CASE("Interpolation Evaluation (w4)", "[interpolations][short]") {
+    const int Nr = 100;
+    const int Nt = 50;
+    const double dr = 1.0 / Nr;
+    const double dt = 2 * M_PI / Nt;
+
+    for(int i = 0; i < Nr; i++) {
+        for (int j = 0; j < Nt; j++) {
+            const double r = i * dr;
+            const double t = j * dt;
+            CHECK(w4_naive(r, t) == Approx(w4_horner(r, t)));
+        }
+    }
+}
+
+TEST_CASE("Interpolation Evaluation (w5)", "[interpolations][short]") {
+    const int Nr = 100;
+    const int Nt = 50;
+    const double dr = 1.0 / Nr;
+    const double dt = 2 * M_PI / Nt;
+
+    for(int i = 0; i < Nr; i++) {
+        for (int j = 0; j < Nt; j++) {
+            const double r = i * dr;
+            const double t = j * dt;
+            CHECK(w5_naive(r, t) == Approx(w5_horner(r, t)));
+        }
+    }
+}
+
+TEST_CASE("Interpolation Evaluation (w6)", "[interpolations][short]") {
+    const int Nr = 100;
+    const int Nt = 50;
+    const double dr = 1.0 / Nr;
+    const double dt = 2 * M_PI / Nt;
+
+    for(int i = 0; i < Nr; i++) {
+        for (int j = 0; j < Nt; j++) {
+            const double r = i * dr;
+            const double t = j * dt;
+            CHECK(w6_naive(r, t) == Approx(w6_horner(r, t)));
+        }
+    }
+}
